a95f7b5027fb0b4e0675b92133a3803946645201
cmacphee <c.macphee94@gmail.com>
2021-09-29 16:33:49 +0100

    Functions demo file

--- Javascript/Script.js
+++ Javascript/Script.js
@@ -1,54 +1,52 @@
-//SCOPE
-
-//Function scope
-function foo() {
-    let bar = 'Hello World';
-    console.log(bar);
+//Functions
+//structure
+function funcName(arg1, arg2, arg3) {
+    //execute some code
 };
-foo();
-//bar was declared inside function so cannot be called outside. Local scope
-// console.log(bar);
 
-
-//This is a block. block scope. again cant access outwith block
-{
-    let x = 1;
+//sum - pass in variable
+let x = sum(3, 5);
+function sum(a,b) {
+    return a + b;
 }
-// console.log(x)
-
-//var is not good as it allows you to access. bad practice.
-{
-    var y = 3;
-};
-console.log(y);
-
-//Global scope - accessible outside the function if function is run first
-
-function global() {
-greeting = 'Heya';
-};
-global();
-console.log(greeting)
-
-//standard global - globally scoped so can be called 
-//outside and inside of functions
-let a = 'a';
-console.log(a)
-
-function b() {
-    console.log(`a = ${a}`);
-};
-
-//order of scope:
-//Local (inside the block)
-//global (outside the block)
-// if none exists add to the global scope
-
-//SCOPE TUTORIAL
+console.log(x);
 
 
+//Different types of functions - all do the same thing
 
+let  welcome = function(){
+    console.log('hello');
+}
 
+welcome();
 
+let welcomeTwo = () => {
+    console.log('hello two');
+}
+welcomeTwo();
+
+welcomeThree = () => console.log('hello three');
+welcomeThree();
+
+welcomeName = fullName => console.log(`hello ${fullName}`);
+welcomeName('Cameron');
+
+//Functions Tutorials
+
+let d = sum(99, 69);
+function sum(a,b) {
+    return a - b;
+}
+console.log(d);
+
+aboutMe = (myName, age) => (
+    console.log(`My name is ${myName}, I am ${age} years old`)
+);
+aboutMe('Connor', 26);
+
+powerUp = (n1, n2) => (
+    console.log(Math.pow(n1,n2))
+);
+powerUp(4,6);
 
 
